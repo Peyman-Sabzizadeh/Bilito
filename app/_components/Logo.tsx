386a9f74065221d678e7cbaec9582222c8390113
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-06-22 00:18:21 +0330

    fix(logo): make navigable

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Image
-      src="logo.svg"
-      alt="Logo"
-      width={148}
-      height={48}
-      priority
-      style={{ width: "130px", height: "auto" }}
-    />
+    <Link href="/">
+      <Image
+        src="logo.svg"
+        alt="Logo"
+        width={148}
+        height={48}
+        priority
+        style={{ width: "130px", height: "auto" }}
+      />
+    </Link>
   );
 }
