8e95665cfd43cc1adea7f12e7cdaea92646f8eff
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-06-24 23:19:56 +0330

    fix(header): convert mobile menu section to a component

--- app/_components/header/components/MobileHeader.tsx
+++ app/_components/header/components/MobileHeader.tsx
@@ -1,9 +1,12 @@
 import Logo from "@/_components/Logo";
+import MobileMenu from "./MobileMenu";
 
 export default function MobileHeader() {
   return (
     <div className="flex justify-between items-center">
-      <div>menu</div>
+      <div>
+        <MobileMenu />
+      </div>
       <div className="w-28 h-auto">
         <Logo />
       </div>
