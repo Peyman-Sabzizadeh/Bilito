8f89be34e490e7b1071dc8ae8e228f6d12498133
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-08-10 08:28:27 +0330

    refactor(header): remove register dropdown

--- app/_components/header/components/desktop/DesktopHeader.tsx
+++ app/_components/header/components/desktop/DesktopHeader.tsx
@@ -2,13 +2,6 @@ import Logo from "@/_components/Logo";
 import DesktopMenuItems from "./DesktopMenuItems";
 import SupportLink from "../SupportLink";
 import RegisterButton from "../RegisterButton";
-import { Plane } from "lucide-react";
-import {
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownPopover,
-} from "@heroui/react";
 
 export default function DesktopHeader() {
   return (
@@ -19,21 +12,7 @@ export default function DesktopHeader() {
       </div>
       <div className="flex items-center gap-6">
         <SupportLink size={18} />
-        <Dropdown>
-          <RegisterButton withIcon className="py-2 hover:bg-[#11577A]" />
-          <DropdownPopover className="min-w-0 p-1 rounded-xl">
-            <DropdownMenu className="*:rounded-lg *:text-[#606060]">
-              <DropdownItem href="/">
-                <Plane size={20} />
-                گزینه 1
-              </DropdownItem>
-              <DropdownItem href="/">
-                <Plane size={20} />
-                گزینه 2
-              </DropdownItem>
-            </DropdownMenu>
-          </DropdownPopover>
-        </Dropdown>
+        <RegisterButton withIcon className="py-2 hover:bg-[#11577A]" />
       </div>
     </div>
   );
