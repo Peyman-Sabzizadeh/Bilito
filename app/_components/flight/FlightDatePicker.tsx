0bf5370ceadc2b18c8faf4cf34c8c1ed0ec826b1
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-08-31 23:06:51 +0330

    fix(flight): solve popover size issue in flight date picker component

--- app/_components/flight/FlightDatePicker.tsx
+++ app/_components/flight/FlightDatePicker.tsx
@@ -6,7 +6,6 @@ import { ChevronDown } from "lucide-react";
 
 export default function FlightDatePicker() {
   const [date, setDate] = useState<DateValue | null>(null);
-  console.log("Date choosen: ", date);
   return (
     <DatePicker value={date} onChange={setDate} aria-label="Flight date picker">
       <DateField.Group className="border-gray-3 h-14 rounded-lg border px-1 shadow-none active:border-none md:w-35">
@@ -27,7 +26,7 @@ export default function FlightDatePicker() {
           </DatePicker.Trigger>
         </DateField.Suffix>
       </DateField.Group>
-      <DatePicker.Popover>
+      <DatePicker.Popover className="max-w-none">
         <Calendar className="ml-13" aria-label="Flight date">
           <Calendar.Header>
             <Calendar.YearPickerTrigger>
