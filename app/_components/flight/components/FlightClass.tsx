5f5b2fac4d07a09dea087b85d79782be84123e36
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-09-11 11:14:02 +0330

    feat(flight): add trigger of FlightClass component

--- app/_components/flight/components/FlightClass.tsx
+++ app/_components/flight/components/FlightClass.tsx
@@ -2,10 +2,10 @@ import { ListBox, Select } from "@heroui/react";
 
 export default function FlightClass() {
   return (
-    <Select aria-label="Flight class">
-      <Select.Trigger>
+    <Select aria-label="Flight class" placeholder="کلاس پرواز">
+      <Select.Trigger className="border-gray-3 *:text-gray-8 flex h-14 cursor-pointer items-center justify-between rounded-lg border *:text-sm *:max-md:font-medium md:w-35">
         <Select.Value />
-        <Select.Indicator />
+        <Select.Indicator className="ml-1 md:hidden" />
       </Select.Trigger>
       <Select.Popover>
         <ListBox aria-label="Flight class items">
