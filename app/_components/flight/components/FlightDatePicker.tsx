2fa2d5c11cf182f73bd9d7f50ca3559651ea294a
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-09-16 13:53:30 +0330

    fix(flight): set Persian locale language for FlightDatePicker and FlightDateRangePicker components

--- app/_components/flight/components/FlightDatePicker.tsx
+++ app/_components/flight/components/FlightDatePicker.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { Calendar, DateField, DatePicker, DateValue } from "@heroui/react";
+import {
+  Calendar,
+  DateField,
+  DatePicker,
+  DateValue,
+  I18nProvider,
+} from "@heroui/react";
 import { toJalaali } from "jalaali-js";
 import { ChevronDown } from "lucide-react";
 import { useRef, useState } from "react";
@@ -10,61 +16,63 @@ export default function FlightDatePicker() {
   const persisanDate = date ? toJalaali(date.year, date.month, date.day) : null;
   const triggerRef = useRef<HTMLButtonElement>(null);
   return (
-    <DatePicker
-      value={date}
-      onChange={setDate}
-      aria-label="Flight date picker"
-      className="w-full flex-1"
-    >
-      <DateField.Group
-        onClick={() => triggerRef.current?.click()}
-        className="border-gray-3 h-14 cursor-pointer justify-between rounded-lg border px-1 shadow-none md:h-12"
+    <I18nProvider locale="fa-IR-u-ca-persian">
+      <DatePicker
+        value={date}
+        onChange={setDate}
+        aria-label="Flight date picker"
+        className="w-full flex-1"
       >
-        {!date ? (
-          <div className="text-gray-8 mr-2 w-full max-md:font-medium">
-            تاریخ رفت
-          </div>
-        ) : (
-          <div className="text-gray-8 mr-2 max-md:font-medium" dir="ltr">
-            {`${persisanDate?.jy} / ${String(persisanDate?.jm).padStart(
-              2,
-              "0",
-            )} / ${String(persisanDate?.jd).padStart(2, "0")}`}
-          </div>
-        )}
-        <DateField.Suffix>
-          <DatePicker.Trigger ref={triggerRef}>
-            <DatePicker.TriggerIndicator className="text-gray-8 size-5">
-              <ChevronDown size={18} className="md:hidden" />
-            </DatePicker.TriggerIndicator>
-          </DatePicker.Trigger>
-        </DateField.Suffix>
-      </DateField.Group>
-      <DatePicker.Popover className="max-w-none">
-        <Calendar className="ml-13" aria-label="Flight date">
-          <Calendar.Header>
-            <Calendar.YearPickerTrigger>
-              <Calendar.YearPickerTriggerHeading />
-              <Calendar.YearPickerTriggerIndicator />
-            </Calendar.YearPickerTrigger>
-            <Calendar.NavButton slot="next" />
-            <Calendar.NavButton slot="previous" />
-          </Calendar.Header>
-          <Calendar.Grid>
-            <Calendar.GridHeader>
-              {(day) => <Calendar.HeaderCell>{day}</Calendar.HeaderCell>}
-            </Calendar.GridHeader>
-            <Calendar.GridBody>
-              {(date) => <Calendar.Cell date={date} />}
-            </Calendar.GridBody>
-          </Calendar.Grid>
-          <Calendar.YearPickerGrid>
-            <Calendar.YearPickerGridBody>
-              {({ year }) => <Calendar.YearPickerCell year={year} />}
-            </Calendar.YearPickerGridBody>
-          </Calendar.YearPickerGrid>
-        </Calendar>
-      </DatePicker.Popover>
-    </DatePicker>
+        <DateField.Group
+          onClick={() => triggerRef.current?.click()}
+          className="border-gray-3 h-14 cursor-pointer justify-between rounded-lg border px-1 shadow-none md:h-12"
+        >
+          {!date ? (
+            <div className="text-gray-8 mr-2 w-full max-md:font-medium">
+              تاریخ رفت
+            </div>
+          ) : (
+            <div className="text-gray-8 mr-2 max-md:font-medium" dir="ltr">
+              {`${persisanDate?.jy} / ${String(persisanDate?.jm).padStart(
+                2,
+                "0",
+              )} / ${String(persisanDate?.jd).padStart(2, "0")}`}
+            </div>
+          )}
+          <DateField.Suffix>
+            <DatePicker.Trigger ref={triggerRef}>
+              <DatePicker.TriggerIndicator className="text-gray-8 size-5">
+                <ChevronDown size={18} className="md:hidden" />
+              </DatePicker.TriggerIndicator>
+            </DatePicker.Trigger>
+          </DateField.Suffix>
+        </DateField.Group>
+        <DatePicker.Popover className="w-80 max-w-none">
+          <Calendar aria-label="Flight date" className="w-auto max-w-none">
+            <Calendar.Header>
+              <Calendar.YearPickerTrigger>
+                <Calendar.YearPickerTriggerHeading />
+                <Calendar.YearPickerTriggerIndicator />
+              </Calendar.YearPickerTrigger>
+              <Calendar.NavButton slot="next" />
+              <Calendar.NavButton slot="previous" />
+            </Calendar.Header>
+            <Calendar.Grid>
+              <Calendar.GridHeader>
+                {(day) => <Calendar.HeaderCell>{day}</Calendar.HeaderCell>}
+              </Calendar.GridHeader>
+              <Calendar.GridBody>
+                {(date) => <Calendar.Cell date={date} />}
+              </Calendar.GridBody>
+            </Calendar.Grid>
+            <Calendar.YearPickerGrid>
+              <Calendar.YearPickerGridBody>
+                {({ year }) => <Calendar.YearPickerCell year={year} />}
+              </Calendar.YearPickerGridBody>
+            </Calendar.YearPickerGrid>
+          </Calendar>
+        </DatePicker.Popover>
+      </DatePicker>
+    </I18nProvider>
   );
 }
--- app/_components/flight/components/FlightDateRangePicker.tsx
+++ app/_components/flight/components/FlightDateRangePicker.tsx
@@ -5,6 +5,7 @@ import {
   DateField,
   DateRangePicker,
   DateValue,
+  I18nProvider,
   RangeCalendar,
   RangeValue,
 } from "@heroui/react";
@@ -27,76 +28,80 @@ export default function FlightDateRangePicker() {
     : null;
   const triggerRef = useRef<HTMLButtonElement>(null);
   return (
-    <DateRangePicker
-      value={rangeDate}
-      onChange={setRangeDate}
-      endName="endDate"
-      startName="startDate"
-      aria-label="Flight date range picker"
-      className="w-full flex-2"
-    >
-      <DateField.Group
-        onClick={() => triggerRef.current?.click()}
-        className="border-gray-3 h-14 cursor-pointer justify-between rounded-lg border px-1 shadow-none md:h-12"
+    <I18nProvider locale="fa-IR-u-ca-persian">
+      <DateRangePicker
+        value={rangeDate}
+        onChange={setRangeDate}
+        endName="endDate"
+        startName="startDate"
+        aria-label="Flight date range picker"
+        className="w-full flex-2"
       >
-        {!rangeDate ? (
-          <div className="text-gray-8 mr-2 w-full max-md:font-medium">
-            تاریخ رفت و برگشت
-          </div>
-        ) : (
-          <div className="flex gap-2 pr-2">
-            <div className="text-gray-8 max-md:font-medium" dir="ltr">
-              {`${startPersisanDate?.jy} / ${String(
-                startPersisanDate?.jm,
-              ).padStart(
-                2,
-                "0",
-              )} / ${String(startPersisanDate?.jd).padStart(2, "0")}`}
+        <DateField.Group
+          onClick={() => triggerRef.current?.click()}
+          className="border-gray-3 h-14 cursor-pointer justify-between rounded-lg border px-1 shadow-none md:h-12"
+        >
+          {!rangeDate ? (
+            <div className="text-gray-8 mr-2 w-full max-md:font-medium">
+              تاریخ رفت و برگشت
             </div>
-            <DateRangePicker.RangeSeparator />
-            <div className="text-gray-8 max-md:font-medium" dir="ltr">
-              {`${endPersisanDate?.jy} / ${String(endPersisanDate?.jm).padStart(
-                2,
-                "0",
-              )} / ${String(endPersisanDate?.jd).padStart(2, "0")}`}
+          ) : (
+            <div className="flex gap-2 pr-2">
+              <div className="text-gray-8 max-md:font-medium" dir="ltr">
+                {`${startPersisanDate?.jy} / ${String(
+                  startPersisanDate?.jm,
+                ).padStart(
+                  2,
+                  "0",
+                )} / ${String(startPersisanDate?.jd).padStart(2, "0")}`}
+              </div>
+              <DateRangePicker.RangeSeparator />
+              <div className="text-gray-8 max-md:font-medium" dir="ltr">
+                {`${endPersisanDate?.jy} / ${String(
+                  endPersisanDate?.jm,
+                ).padStart(
+                  2,
+                  "0",
+                )} / ${String(endPersisanDate?.jd).padStart(2, "0")}`}
+              </div>
             </div>
-          </div>
-        )}
-        <DateField.Suffix>
-          <DateRangePicker.Trigger ref={triggerRef}>
-            <DateRangePicker.TriggerIndicator className="text-gray-8 size-5">
-              <ChevronDown size={18} className="md:hidden" />
-            </DateRangePicker.TriggerIndicator>
-          </DateRangePicker.Trigger>
-        </DateField.Suffix>
-      </DateField.Group>
-      <DateRangePicker.Popover className="w-80 max-w-none">
-        <RangeCalendar aria-label="Trip dates" className="w-auto max-w-none">
-          <RangeCalendar.Header>
-            <RangeCalendar.YearPickerTrigger>
-              <RangeCalendar.YearPickerTriggerHeading />
-              <RangeCalendar.YearPickerTriggerIndicator />
-            </RangeCalendar.YearPickerTrigger>
-            <RangeCalendar.NavButton slot="previous" />
-            <RangeCalendar.NavButton slot="next" />
-          </RangeCalendar.Header>
-          <RangeCalendar.Grid>
-            <RangeCalendar.GridHeader>
-              {(day) => (
-                <RangeCalendar.HeaderCell>{day}</RangeCalendar.HeaderCell>
-              )}
-            </RangeCalendar.GridHeader>
-            <RangeCalendar.GridBody>
-              {(date) => <RangeCalendar.Cell date={date} />}
-            </RangeCalendar.GridBody>
-          </RangeCalendar.Grid>
-          <RangeCalendar.YearPickerGrid>
-            <RangeCalendar.YearPickerGridBody>
-              {({ year }) => <RangeCalendar.YearPickerCell year={year} />}
-            </RangeCalendar.YearPickerGridBody>
-          </RangeCalendar.YearPickerGrid>
-        </RangeCalendar>
-      </DateRangePicker.Popover>
-    </DateRangePicker>
+          )}
+          <DateField.Suffix>
+            <DateRangePicker.Trigger ref={triggerRef}>
+              <DateRangePicker.TriggerIndicator className="text-gray-8 size-5">
+                <ChevronDown size={18} className="md:hidden" />
+              </DateRangePicker.TriggerIndicator>
+            </DateRangePicker.Trigger>
+          </DateField.Suffix>
+        </DateField.Group>
+        <DateRangePicker.Popover className="w-80 max-w-none">
+          <RangeCalendar aria-label="Flight dates" className="w-auto max-w-none">
+            <RangeCalendar.Header>
+              <RangeCalendar.YearPickerTrigger>
+                <RangeCalendar.YearPickerTriggerHeading />
+                <RangeCalendar.YearPickerTriggerIndicator />
+              </RangeCalendar.YearPickerTrigger>
+              <RangeCalendar.NavButton slot="next" />
+              <RangeCalendar.NavButton slot="previous" />
+            </RangeCalendar.Header>
+            <RangeCalendar.Grid>
+              <RangeCalendar.GridHeader>
+                {(day) => (
+                  <RangeCalendar.HeaderCell>{day}</RangeCalendar.HeaderCell>
+                )}
+              </RangeCalendar.GridHeader>
+              <RangeCalendar.GridBody>
+                {(date) => <RangeCalendar.Cell date={date} />}
+              </RangeCalendar.GridBody>
+            </RangeCalendar.Grid>
+            <RangeCalendar.YearPickerGrid>
+              <RangeCalendar.YearPickerGridBody>
+                {({ year }) => <RangeCalendar.YearPickerCell year={year} />}
+              </RangeCalendar.YearPickerGridBody>
+            </RangeCalendar.YearPickerGrid>
+          </RangeCalendar>
+        </DateRangePicker.Popover>
+      </DateRangePicker>
+    </I18nProvider>
   );
 }
