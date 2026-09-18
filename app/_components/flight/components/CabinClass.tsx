73da67f8503e419ea6088d50edd2ea70fd860fa2
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-09-19 00:41:13 +0330

    refactor(flight): connect CabinClass component to zustand

--- app/_components/flight/components/CabinClass.tsx
+++ app/_components/flight/components/CabinClass.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { ListBox, Select } from "@heroui/react";
 import ClassListBoxItem from "./ClassListBoxItem";
+import { useFlightSearch } from "@/_store/flightSearchStore";
 
 export default function CabinClass() {
+  const cabinClass = useFlightSearch((state) => state.cabinClass);
+  const setCabinClass = useFlightSearch((state) => state.setCabinClass);
   return (
     <Select
       aria-label="Cabin class"
       placeholder="کلاس پرواز"
       className="w-full flex-1"
+      value={cabinClass}
+      onChange={(value) => setCabinClass(value as typeof cabinClass)}
     >
       <Select.Trigger className="border-gray-3 *:text-gray-8 flex h-14 cursor-pointer items-center justify-between rounded-lg border shadow-none *:text-sm *:max-md:font-medium md:h-12">
         <Select.Value />
