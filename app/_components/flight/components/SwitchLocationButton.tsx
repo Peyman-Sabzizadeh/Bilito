e7f5b7dc6fd1e63bee78aaf22f8f23b4b0370109
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-09-19 20:14:11 +0330

    feat(flight): complete SwitchLocationButton component

--- app/_components/flight/components/SwitchLocationButton.tsx
+++ app/_components/flight/components/SwitchLocationButton.tsx
@@ -1,11 +1,22 @@
+"use client";
+
+import { useFlightSearch } from "@/_store/flightSearchStore";
 import { Button } from "@heroui/react";
 import { ArrowRightLeft } from "lucide-react";
 
 export default function SwitchLocationButton() {
+  const origin = useFlightSearch((state) => state.origin);
+  const destination = useFlightSearch((state) => state.destination);
+  const setOrigin = useFlightSearch((state) => state.setOrigin);
+  const setDestination = useFlightSearch((state) => state.setDestination);
   return (
     <Button
       isIconOnly
       className="text-gray-9 size-4 bg-transparent hover:bg-transparent max-md:hidden"
+      onPress={() => {
+        setOrigin(destination);
+        setDestination(origin);
+      }}
     >
       <ArrowRightLeft />
     </Button>
