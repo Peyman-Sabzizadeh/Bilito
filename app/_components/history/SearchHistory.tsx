efe856f0bd9132eeff2e934392e0e248dc340c39
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-09-24 22:43:28 +0330

    feat(history): add EmptyHistory component

--- app/_components/history/SearchHistory.tsx
+++ app/_components/history/SearchHistory.tsx
@@ -6,26 +6,23 @@ import { flightRoutes } from "@/_data/flightRoutes";
 import NavigationButton from "./components/NavigationButton";
 import SearchHistoryHeader from "./components/SearchHistoryHeader";
 import HistorySwiper from "./components/HistorySwiper";
+import EmptyHistory from "./components/EmptyHistory";
 
 export default function SearchHistory() {
   const [nextEl, setNextEl] = useState<HTMLButtonElement | null>(null);
   const [prevEl, setPrevEl] = useState<HTMLButtonElement | null>(null);
-
   const [history, setHistory] = useState(flightRoutes);
-
   const removeItem = (id: number) => {
     setHistory((prev) => prev.filter((item) => item.id !== id));
   };
-
   const clearAll = () => {
     setHistory([]);
   };
-
   return (
     <Container className="flex flex-col gap-4 pt-6">
       <SearchHistoryHeader clearAll={clearAll} />
       {!history.length ? (
-        "تاریخچه ای وجود ندارد!"
+        <EmptyHistory />
       ) : (
         <div className="flex gap-9">
           <NavigationButton type="prev" setElement={setPrevEl} />
---
+++ app/_components/history/components/EmptyHistory.tsx
@@ -0,0 +1,7 @@
+import { Description } from "@heroui/react";
+
+export default function EmptyHistory() {
+  return (
+    <Description className="text-center">تاریخچه ای وجود ندارد!</Description>
+  );
+}
