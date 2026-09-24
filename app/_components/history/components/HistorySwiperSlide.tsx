491c3ae7c5a4e85b186e7eed352ebb81380c8cfc
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-09-24 20:20:55 +0330

    refactor(history): extract RouteButton component

--- app/_components/history/components/HistorySwiperSlide.tsx
+++ app/_components/history/components/HistorySwiperSlide.tsx
@@ -1,4 +1,5 @@
 import RemoveItemButton from "./RemoveItemButton";
+import RouteButton from "./RouteButton";
 
 type HistorySwiperSlideProps = {
   item: {
@@ -16,9 +17,7 @@ export default function HistorySwiperSlide({
   return (
     <div className="border-gray-3 flex cursor-pointer items-center gap-2 rounded-md border px-3 py-2">
       <RemoveItemButton removeItem={removeItem} itemId={item.id} />
-      <span className="text-gray-7 text-xs font-medium select-none">
-        {item.from} به {item.to}
-      </span>
+      <RouteButton item={item} />
     </div>
   );
 }
---
+++ app/_components/history/components/RouteButton.tsx
@@ -0,0 +1,26 @@
+import { useFlightSearch } from "@/_store/flightSearchStore";
+import { Button } from "@heroui/react";
+
+type RouteButtonProps = {
+  item: {
+    id: number;
+    from: string;
+    to: string;
+  };
+};
+
+export default function RouteButton({ item }: RouteButtonProps) {
+  const setOrigin = useFlightSearch((state) => state.setOrigin);
+  const setDestination = useFlightSearch((state) => state.setDestination);
+  return (
+    <Button
+      onClick={() => {
+        setOrigin(item.from);
+        setDestination(item.to);
+      }}
+      className="text-gray-7 size-auto bg-transparent p-0 text-xs font-medium select-none"
+    >
+      {item.from} به {item.to}
+    </Button>
+  );
+}
