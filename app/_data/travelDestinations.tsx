ea97ed0b7d1cd22bb426f5c0c6e775f230613996
Peyman Sabzizadeh <peyman.front.developer@gmail.com>
2026-09-25 23:19:29 +0330

    feat(destinations): add travelDestinations file

---
+++ app/_data/travelDestinations.tsx
@@ -0,0 +1,12 @@
+export type TravelDestination = {
+  destination: string;
+  persianDestination: string;
+  title: string;
+};
+
+export const travelDestinations: TravelDestination[] = [
+  { destination: "kish", persianDestination: "کیش", title: "بهترین فصل شنا" },
+  { destination: "turkey", persianDestination: "ترکیه", title: "سفر به ترکیه" },
+  { destination: "shiraz", persianDestination: "شیراز", title: "دنیای از تاریخ و هنر" },
+  { destination: "dubai", persianDestination: "دبی", title: "شگفتی در صحرا" },
+];
